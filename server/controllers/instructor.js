61c450c834ecac8b512b84c80cd2eaa0150f47c5
Vivek Singh <viveksingh27795@gmail.com>
2022-03-15 12:04:46 +0530

    payment complete

--- server/controllers/instructor.js
+++ server/controllers/instructor.js
@@ -1,5 +1,5 @@
 import User from "../models/user"
-import querystring from  "query-string"
+import queryString from  "query-string"
 const stripe =require("stripe")(process.env.STRIPE_SECRET);
 
 export const makeInstructor = async (req,res) =>{
@@ -13,38 +13,36 @@ export const makeInstructor = async (req,res) =>{
     //New User
     const user = await User.findById(req.user._id).exec();
     console.log(user);
-    if(!user.stripe_account_id){
-        const account=await stripe.accounts.create({
-            type: 'custom',
-      business_type: 'individual',
-      requested_capabilities: ['card_payments', 'transfers'],
-        
-        });
-        //console.log("Acc ->",account.id);
-        user.stripe_account_id=account.id;
-        user.save();
-
-    }
-    //Linking
-    // let accountLink= await stripe.accountLinks.create({
-        
-    //     account:user.stripe_account_id,
-    //     refresh_url:process.env.STRIPE_REDIRECT_URL,
-    //     return_url:process.env.STRIPE_REDIRECT_URL,
-    //     type: 'custom_account_verification',
-    //     collect: 'eventually_due',
-    // });
-    let accountLink=process.env.STRIPE_REDIRECT_URL
   
-    console.log("Account Link => ",accountLink)
-
-    //Send link to client
-
-    res.send(`${accountLink}`);
-   }catch(err){
-       console.log(err);
-   }
-
+        const session = await stripe.checkout.sessions.create({
+            line_items: [
+              {
+                price_data: {
+                  currency: 'inr',
+                  product_data: {
+                    name: 'T-shirt',
+                  },
+                  unit_amount: 2000,
+                },
+                quantity: 1,
+              },
+            ],
+            mode: 'payment',
+           
+            success_url: process.env.STRIPE_REDIRECT_URL,
+            cancel_url: process.env.STRIPE_REDIRECT_URL,
+          });
+         
+          user.stripe_account_id=session.payment_intent;
+            user.save();
+            console.log(session);
+            console.log(`${session.url}`);
+            res.send(`${session.url}`);
+    }
+ 
+catch (err) {
+  console.log("MAKE INSTRUCTOR ERR ", err);
+}
 
 }
 
@@ -54,8 +52,8 @@ export const getAccountStatus = async (req,res) =>{
      
      const user = await User.findById(req.user._id).exec();
      
-     let account =await stripe.accounts.retrieve(user.stripe_account_id);
-     account.charges_enabled=true
+     let account =await stripe.paymentIntents.retrieve(user.stripe_account_id);
+    // account.charges_enabled=true
      const statusUpdated=await User.findByIdAndUpdate(user._id,{
          stripe_seller:account,
          $addToSet:{role:'Instructor'},
